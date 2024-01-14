cc7aa352f8f5772f431aa9939c5a8dca9e6eedf8
RakeshAkaram <rakesh.akaram@gmail.com>
2024-01-14 12:42:02 +0530

    Initial Commit

--- src/graphs/AdjacencyMatrix.java
+++ src/graphs/AdjacencyMatrix.java
@@ -6,7 +6,7 @@ public class AdjacencyMatrix {
 
 	public static void main(String[] args) {
 		
-		System.out.println("I started working on Graph Questionss");
+		System.out.println("I started working on Graph Questions");
 
 	}
 
