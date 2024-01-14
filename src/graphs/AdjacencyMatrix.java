a6f130605887b8122ed0507c097c767463f1717f
RakeshAkaram <rakesh.akaram@gmail.com>
2024-01-14 12:32:14 +0530

    a

---
+++ src/graphs/AdjacencyMatrix.java
@@ -0,0 +1,16 @@
+package graphs;
+
+
+
+public class AdjacencyMatrix {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I started working on Graph Questionss");
+
+	}
+
+}
+
+
+
