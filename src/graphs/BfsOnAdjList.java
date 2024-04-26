f9d6b7362a8c82d8f4f457476e5b42a4298fa0e0
RakeshAkaram <rakesh.akaram@gmail.com>
2024-04-27 01:27:43 +0530

    Added getData method to take the data from the console.

--- src/graphs/BfsOnAdjList.java
+++ src/graphs/BfsOnAdjList.java
@@ -1,20 +1,22 @@
 package graphs;
 
+import java.security.Key;
 import java.util.*;
 
 public class BfsOnAdjList {
     static Map<Integer,ArrayList<Integer>> adjList=new HashMap<>();
     static Map<Integer,Boolean> visited=new HashMap<>();
     public static void main(String[] args) {
+        getData();
         //Consider the start Index is 1
         int startNode=1;
         Queue<Integer> bfsQueue=new LinkedList<>();
         bfsQueue.add(startNode);
         visited.put(startNode,true);
         while(!bfsQueue.isEmpty()){
-            Integer nodeTofindItsAdj=bfsQueue.poll();
-            System.out.print(nodeTofindItsAdj+" ");
-            for(Integer adjNode: adjList.get(nodeTofindItsAdj)){
+            Integer nodeFindItsAdj=bfsQueue.poll();
+            System.out.print(nodeFindItsAdj+" ");
+            for(Integer adjNode: adjList.get(nodeFindItsAdj)){
                 if(!visited.get(adjNode)){
                     bfsQueue.add(adjNode);
                     visited.put(adjNode,true);
@@ -22,36 +24,36 @@ public class BfsOnAdjList {
             }
         }
     }
-    // To Prepare the adjacency Dummy  List
-    // 1--> 2,3
-    // 2--> 1,4
-    // 3--> 1,4
-    // 4--> 2,3
-    static {
-        System.out.println("In Static");
-        ArrayList<Integer> node1=new ArrayList<>();
-        //node1.add(2);
-        node1.add(3);
-        adjList.put(1,node1);
-        visited.put(1,false);
+   static void  getData(){
+        Scanner sc=new Scanner(System.in);
+        List<int []> data=new ArrayList<>();
+        System.out.println("Enter the number of the Edges:");
+        int edges=sc.nextInt();
 
-        ArrayList<Integer> node2=new ArrayList<>();
-        //node2.add(1);
-        node2.add(4);
-        adjList.put(2,node2);
-        visited.put(2,false);
-
-        ArrayList<Integer> node3=new ArrayList<>();
-        node3.add(1);
-        node3.add(4);
-        adjList.put(3,node3);
-        visited.put(3,false);
-
-        ArrayList<Integer> node4=new ArrayList<>();
-        node4.add(2);
-        node4.add(3);
-        adjList.put(4,node4);
-        visited.put(4,false);
-
+        System.out.println("Enter the connected edge data as U V");
+        while(edges>0){
+            int u=sc.nextInt();
+            int v=sc.nextInt();
+            data.add(new int[]{u,v});
+            edges--;
+        }
+        // Convert the data to
+        for (int [] p: data){
+            System.out.println("Edges Entered");
+            System.out.println(p[0] +"-->"+ p[1]);
+            if(adjList.get(p[0])!=null){
+                adjList.get(p[0]).add(p[1]);
+            }
+            else {
+                ArrayList<Integer> node=new ArrayList<>();
+                node.add(p[1]);
+                adjList.put(p[0],node);
+            }
+            visited.put(p[0],false);
+            visited.put(p[1],false);
+        }
+        for(Map.Entry<Integer,ArrayList<Integer>> map: adjList.entrySet()){
+            System.out.println(map.getKey() +" : "+map.getValue());
+        }
     }
 }
